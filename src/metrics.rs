1e2104c75fc24b17325a5900c1daf9ff90dcc1f2
Peter Grace <pete.grace@gmail.com>
2020-10-22 06:51:50 -0400

    use register_gauge_vec macro to make and register the static metric

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,13 +1,13 @@
 use once_cell::sync::Lazy;
-use prometheus::{opts, GaugeVec};
+use prometheus::{opts, register_gauge_vec, GaugeVec};
 
 pub(crate) static APP_INFO: Lazy<GaugeVec> = Lazy::new(|| {
-    GaugeVec::new(
+    register_gauge_vec!(
         opts!(
             "app_info",
             "static app labels that potentially only change at restart"
         ),
-        &["app_name", "crate_version", "git_hash"],
+        &["app_name", "crate_version", "git_hash"]
     )
     .expect("Could not create lazy GaugeVec for app_info metric")
 });
